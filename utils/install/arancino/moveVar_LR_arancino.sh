2dcc1c7002b765248b7d742e8eac5debd498d242
Mimmo <mimmo@smartme.io>
2018-02-21 16:05:22 +0000

    Make more atomic the changes

--- utils/install/arancino/moveVar_LR_arancino.sh
+++ utils/install/arancino/moveVar_LR_arancino.sh
@@ -3,12 +3,17 @@
 
 echo "Moving /var/ folder..."
 rm /var
-cd /
-mkdir var
-cp -a /tmp/etc/ /var/
-cp -a /tmp/lib/ /var/
-cp -a /tmp/log/ /var/
-cp -a /tmp/run/ /var/
+mkdir /var
+mv /tmp/etc /var
+mv /tmp/lib /var
+mv /tmp/log /var
+mv /tmp/run /var
+#cd /
+#mkdir var
+#cp -a /tmp/etc/ /var/
+#cp -a /tmp/lib/ /var/
+#cp -a /tmp/log/ /var/
+#cp -a /tmp/run/ /var/
 echo " - /var/ folder moved."
 
 
